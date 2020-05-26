42573d7dbd2fb3e6ae2bd280ff6a4aaedc471258
mj <hi.minjungkim@gmail.com>
2020-05-26 20:19:47 +0900

    done with emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+/** @jsx jsx */
+import React from "react";
+import { jsx, css } from "@emotion/core";
 
-function App() {
+const currentTime = new Date();
+const year = currentTime.getFullYear();
+const month = currentTime.getMonth() + 1;
+const data = currentTime.getDate();
+
+const App = () => {
   return (
-    <div className="App">
-      div
+    <div css={container}>
+      <div css={time}></div>
     </div>
   );
-}
+};
+
+const container = css`
+  border: 1px solid red;
+  width: 1920px;
+  height: 1080px;
+`;
+
+const time = css`
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
+`;
 
 export default App;
