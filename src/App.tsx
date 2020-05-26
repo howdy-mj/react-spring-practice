d2628b4184038e793e4fb292384414146594a7db
howdy-mj <hi.minjungkim@gmail.com>
2020-05-26 23:56:23 +0900

    dont' know

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { jsx, css } from "@emotion/core";
 import { useSpring, animated } from "react-spring";
+import { Spring } from "react-spring/renderprops";
 
 const currentTime = new Date();
 const CURRENT_YEAR = currentTime.getFullYear();
@@ -33,43 +34,66 @@ const App = () => {
   const [year, setYear] = useState(0);
   const [month, setMonth] = useState(0);
   const [day, setDay] = useState(0);
-  const showTime = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    delay: 1000,
-  });
 
   const moveScroll = () => {
-    if (window.scrollY > HEIGHT * 0.1) {
-      console.log("window.scrollY", window.scrollY);
+    if (window.scrollY > HEIGHT * 0.1 || window.scrollY < HEIGHT * 0.9) {
+      console.log("window.scrollY 날짜 표시", window.scrollY);
       setYear(CURRENT_YEAR);
       setMonth(CURRENT_MONTH);
       setDay(CURRENT_DAY);
-    } else if (window.scrollY < HEIGHT * 0.1) {
+    } else if (window.scrollY < HEIGHT * 0.1 || window.scrollY > HEIGHT * 0.9) {
+      console.log("window.scrollY 날짜 0", window.scrollY);
       setYear(0);
       setMonth(0);
       setDay(0);
-    }
-
-    if (window.scrollY > HEIGHT * 0.9) {
-      console.log("window.scrollY", window.scrollY);
-      setYear(0);
-      setMonth(0);
-      setDay(0);
-    } else if (window.scrollY < HEIGHT * 0.9) {
-      setYear(CURRENT_YEAR);
-      setMonth(CURRENT_MONTH);
-      setDay(CURRENT_DAY);
     }
   };
 
+  // const year = useSpring({ number: CURRENT_YEAR, from: { number: 0 } });
+  // const month = useSpring({ number: CURRENT_MONTH, from: { number: 0 } });
+  // const day = useSpring({ number: CURRENT_DAY, from: { number: 0 } });
+
+  // const showYear = useSpring({
+  //   from: { year: 0 },
+  //   to: { year: CURRENT_YEAR },
+  //   delay: 1000,
+  // });
+
+  // const showMonth = useSpring({
+  //   from: { month: 0 },
+  //   to: { month: CURRENT_MONTH },
+  //   delay: 1000,
+  // });
+
+  // const showDay = useSpring({
+  //   from: { number: 0 },
+  //   to: { number: CURRENT_DAY },
+  //   delay: 1000,
+  // });
+
+  const props = useSpring({
+    to: async (next, cancel) => {
+      await next({ opacity: 1, color: "#ffaaee" });
+      await next({ opacity: 0, color: "rgb(14,26,19)" });
+    },
+    from: { opacity: 0, color: "red" },
+  });
+
   return (
     <div>
+      <animated.div style={props}>I will fade in and out</animated.div>
       <div css={container} onWheel={moveScroll}></div>
       <div css={time}>
-        <animated.span style={showTime}>{year}</animated.span>
-        <animated.span style={showTime}>{month}</animated.span>
-        <animated.span style={showTime}>{day}</animated.span>
+        {/* <animated.span style={showYear}>{year}</animated.span>
+        <animated.span style={showMonth}>{month}</animated.span>
+        <animated.span style={showDay}>{day}</animated.span> */}
+        {/* <Spring
+          config={{ delay: 1000 }}
+          from={{ year: 0 }}
+          to={{ year: CURRENT_YEAR }}
+        >
+          {(props) => <span>{props.year}</span>}
+        </Spring> */}
       </div>
     </div>
   );
