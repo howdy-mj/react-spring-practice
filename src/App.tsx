05b0e12ce4f21b39c1194b031ce457fa3c10a28e
mj <hi.minjungkim@gmail.com>
2020-05-26 20:27:09 +0900

    styling time section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 /** @jsx jsx */
-import React from "react";
+// import React from "react";
 import { jsx, css } from "@emotion/core";
+import { useSpring, animated } from "react-spring";
 
 const currentTime = new Date();
 const year = currentTime.getFullYear();
 const month = currentTime.getMonth() + 1;
-const data = currentTime.getDate();
+const date = currentTime.getDate();
 
 const App = () => {
+  const showTime = useSpring({ opacity: 1, from: { opacity: 0 } });
   return (
     <div css={container}>
-      <div css={time}></div>
+      <div css={time}>
+        <animated.div style={showTime}>{year}</animated.div>
+        <animated.div style={showTime}>{month}</animated.div>
+        <animated.div style={showTime}>{date}</animated.div>
+      </div>
     </div>
   );
 };
@@ -22,9 +28,12 @@ const container = css`
 `;
 
 const time = css`
-  text-align: center;
   font-size: 40px;
   font-weight: bold;
+  display: flex;
+  width: 400px;
+  justify-content: space-between;
+  margin: 0 auto;
 `;
 
 export default App;
